3aea72b064f28f44b1db96338f9371c34a747ba9
yangming <472102644@qq.com>
2019-01-23 18:44:05 +0800

    update materials

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import { createRouter } from './router'
 
+// global-component-start
+Vue.component('ybutton', () => import('@/components/button/index.vue'))
+Vue.component('ytext', () => import('@/components/text/index.vue'))
+// global-component-end
+
 export function createApp () {
   const router = createRouter()
   const app = new Vue({
--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   resolve: {
     alias: {
-      // 'public': path.resolve(__dirname, '../public')
+      '@': path.resolve(__dirname, '../src')
     }
   },
   module: {
